3716a75814570a119c36b9f9e2ad3b2c18b90c47
Michael Jones <JonesyJava@gmail.com>
2021-08-30 14:27:59 -0600

    free-rps init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace free_rps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var choice = "";
+            int playerNum = 0;
+            int compNum = 0;
+
+            while (choice != null)
+            {
+                System.Console.WriteLine("Choose: \n-> Rock \n-> Paper \n-> Scissors");
+                string[] options = new string[3] { "Rock", "Paper", "Scissors" };
+
+                Random random = new Random();
+                int num = random.Next(0, 3);
+                System.Console.WriteLine("What Choice Will Lead To VICTORY?");
+            }
+        }
+    }
+}
